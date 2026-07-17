623c64451fe39853969e2e4938fe95610c4b20da
Pascal-Eric Servais <anynamus@yahoo.ca>
2026-07-17 08:25:20 -0400

    feat(asSequence): adding extension YamlNode.asSequence

--- src/main/scala/sqlgenerator/yaml/YamlNodeOps.scala
+++ src/main/scala/sqlgenerator/yaml/YamlNodeOps.scala
@@ -12,3 +12,8 @@ object YamlNodeOps:
         case YamlNode.Sequence(_) => Left("Cannot map a Sequence node to Scalar")
         case YamlNode.Mapping(_) => Left("Cannot map a Mapping node to Scalar")
 
+    def asSequence: Result[Vector[YamlNode]] =
+      node match
+        case YamlNode.Sequence(values) => Right(values)
+        case YamlNode.Scalar(_) => Left("Cannot map a Scalar node to Sequence")
+        case YamlNode.Mapping(_) => Left("Cannot map a Mapping node to Sequence")
--- src/test/scala/sqlgenerator/yaml/YamlNodeOpsSpec.scala
+++ src/test/scala/sqlgenerator/yaml/YamlNodeOpsSpec.scala
@@ -1,5 +1,8 @@
 package sqlgenerator.yaml
 
+import sqlgenerator.yaml.YamlNode.Scalar
+import sqlgenerator.yaml.YamlNodeOps.asScalar
+import sqlgenerator.yaml.YamlNodeOps.asSequence
 import org.scalatest.funsuite.AnyFunSuite
 
 class YamlNodeOpsSpec extends AnyFunSuite:
@@ -17,4 +20,21 @@ class YamlNodeOpsSpec extends AnyFunSuite:
   test("using asScalar on a Scalar node"):
     YamlNode.Scalar("aScalarValue").asScalar match
       case Right(value) => assert(value == "aScalarValue")
+      case Left(_) => fail("unexpected error")
+
+  test("using asSequence on a Scalar node"):
+    YamlNode.Scalar("a scalar value").asSequence match
+      case Left(error) => assert(error == "Cannot map a Scalar node to Sequence")
+      case Right(_) => fail("error expected")
+
+  test("using asSequence on a Mapping node"):
+    YamlNode.Mapping(Map.empty[String, YamlNode]).asSequence match
+      case Left(error) => assert(error == "Cannot map a Mapping node to Sequence")
+      case Right(_) => fail("error expected")
+
+  test("using asSequence on a Sequence node"):
+    YamlNode.Sequence(Vector(Scalar("first value"))).asSequence match
+      case Right(value) =>
+        assert(value.size == 1)
+        assert(value.head == Scalar("first value"))
       case Left(_) => fail("unexpected error")
